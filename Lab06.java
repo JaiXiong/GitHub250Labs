39e0aed27cc3ac3bc29dad8788ff80fd3c2d60d0
Jai <xiongjai@gmail.com>
2018-04-19 11:25:28 -0500

    cleaned up code

--- Lab06.java
+++ Lab06.java
@@ -9,16 +9,6 @@ public class Lab06 {
 		System.out.println("Enter a number to start:");
 		int n = stdIn.nextInt();
 
-//		for(int i = 1; i <= n; i++)
-//		{	
-//			for (int j = 1; j <= (n-i); j++)
-//			{
-//				System.out.print(" ");
-//			}
-//			for ( int k = i; k >= 1; k--)
-//				System.out.print(k);
-//			System.out.println();
-//		}
 		for (int i = 1; i <= n; i++)
 		{
 			for (int j = 1; j <= (n-i); j++)
